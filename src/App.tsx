ce6384e08e0a5737317b052101a7bd0ce7a702a2
yushagun <yushagun@gmail.com>
2021-10-04 16:46:23 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import { Counter } from './features/counter/Counter';
+import About from './pages/About';
+import Home from './pages/Home';
+
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/app">App</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/app">
+
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Counter />
+                <p>
+                  Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <span>
+                  <span>Learn </span>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    React
+                  </a>
+                  <span>, </span>
+                  <a
+                    className="App-link"
+                    href="https://redux.js.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Redux
+                  </a>
+                  <span>, </span>
+                  <a
+                    className="App-link"
+                    href="https://redux-toolkit.js.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Redux Toolkit
+                  </a>
+                  ,<span> and </span>
+                  <a
+                    className="App-link"
+                    href="https://react-redux.js.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    React Redux
+                  </a>
+                </span>
+              </header>
+            </div>
+
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
