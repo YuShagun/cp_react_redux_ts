21bb9e85d0855c34c4b89193798fe2f11c18db23
yushagun <yushagun@gmail.com>
2021-10-18 16:27:48 +0300

    move header links to material

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@mui/material/Link'
 
 import styles from './Header.module.css';
 
 export default function Header() {
   return (
       <div className='row justify-content-center'>
-        <Link className={styles.link} to="/">Home</Link>
-        <Link className={styles.link} to="/about">About</Link>
-        <Link className={styles.link} to="/app">App</Link>
+        <Link component={RouterLink} underline="none" className={styles.link} to="/">Home</Link>
+        <Link component={RouterLink} underline="none" className={styles.link} to="/about">About</Link>
+        <Link component={RouterLink} underline="none" className={styles.link} to="/app">App</Link>
       </div>
   )
 }
