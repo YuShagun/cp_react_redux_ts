343d5aead9b31cb6195d4dc7c30b96c5c8b628b6
YuShagun <yushagun@gmail.com>
2021-12-05 17:44:39 +0300

    improve header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,23 +10,9 @@ import styles from './Header.module.css';
 export default function Header() {
 
   return (
-    <div className="row">
-      <div className="col"></div>
-      <div className="col">
-        <div className={`row justify-content-center ${styles.linkContainer}`}>
+    <div className={`row justify-content-end align-items-center ${styles.linkContainer}`}>
           <Link component={RouterLink} underline="none" className={styles.link} to="/">Home</Link>
-          <VLine color="var(--dark-cornflower-blue)" />
-          <Link component={RouterLink} underline="none" className={styles.link} to="/about">About</Link>
-          <VLine color="var(--dark-cornflower-blue)" />
-          <Link component={RouterLink} underline="none" className={styles.link} to="/app">App</Link>
-
-        </div>
-      </div>
-      <div className="col">
-        <div className="row justify-content-end align-items-center">
-          <Button variant="outlined" component={RouterLink} to='/actions/upload' style={{ marginRight: '0.5rem' }}>Upload receipt</Button>
-        </div>
-      </div>
+          <Button variant="outlined" color='inherit' component={RouterLink} to='/actions/upload' style={{ marginRight: '2rem' }}>Upload receipt</Button>
     </div>
   )
 }
